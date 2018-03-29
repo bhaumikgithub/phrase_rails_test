5da2a21e161a3ae59dfae2b858ce1c02253a23d2
Bhaumik gadani <bhaumikgithub@gmail.com>
2018-03-30 04:12:43 +0530

    Update database from sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180329205623) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "phrases", force: :cascade do |t|
     t.string   "phrase"
     t.datetime "created_at", null: false
